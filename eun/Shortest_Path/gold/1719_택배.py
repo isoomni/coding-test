7cd5d9f51f2c6fa51561cebdf18fba7176f045f8
EUNJEONGMUN <munej26@gmail.com>
2022-04-12 13:28:01 +0900

    [ADD] #13 1719_EUN_complete 220412 commit

--- eun/Shortest_Path/gold/1719_택배.py
+++ eun/Shortest_Path/gold/1719_택배.py
@@ -1,6 +1,66 @@
 # 1719 택배
 # 2022-04-10
 
+import sys
+import heapq
+input = sys.stdin.readline
+INF = int(1e9)
+
+N, M = map(int, input().split())
+
+result = [[0]*(N+1) for _ in range(N+1)]  # 결과 테이블
+graph = [[] for _ in range(N+1)]
+
+
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))  # 양방향
+    graph[b].append((a, c))
+
+
+def dijkstra(start):
+    q = []
+    # 시작 노드 초기화
+    heapq.heappush(q, (0, start))
+    distance = [[INF, 0] for _ in range(N+1)]
+    distance[start] = [0, 0]
+    result[start][start] = "-"
+
+    for i in graph[start]:
+        # 시작노드에서 처음 도착하는 노드 초기화
+        next_node = i[0]
+        next_node_cost = i[1]
+
+        distance[next_node] = [next_node_cost, next_node]
+        heapq.heappush(q, (next_node_cost, next_node))
+        result[start][next_node] = next_node
+
+    while q:
+        dist, now = heapq.heappop(q)
+
+        for i in graph[now]:
+            next_node = i[0]
+            next_node_cost = i[1]
+
+            cost = distance[now][0]+next_node_cost
+
+            if distance[next_node][0] <= dist:  # 이미 방문한 노드는 continue
+                continue
+
+            if distance[next_node][0] > cost:
+                # 최단 거리 갱신
+                # distance[next_node] 에는 [거리, 이전 노드에 오기까지 가장 먼저 도달한 노드]
+                distance[next_node] = [cost, distance[now][1]]
+                heapq.heappush(q, (cost, next_node))
+                result[start][next_node] = distance[now][1]
+
+
+for i in range(1, N+1):
+    dijkstra(i)
+
+for i in range(1, N+1):
+    print(' '.join(map(str, result[i][1:])))
+
 # 벨만 포드
 # import sys
 # input = sys.stdin.readline
@@ -39,64 +99,3 @@
 # print("------")
 # for i in range(1, N+1):
 #     print(result[i])
-
-
-import sys
-import heapq
-input = sys.stdin.readline
-INF = int(1e9)
-
-N, M = map(int, input().split())
-
-# distance = [[INF]*(N+1) for _ in range(N+1)]
-result = [[0]*(N+1) for _ in range(N+1)]
-graph = [[] for _ in range(N+1)]
-for _ in range(M):
-    a, b, c = map(int, input().split())
-    graph[a].append((b, c))
-
-
-# for i in range(1, N+1):
-#     distance[i][i] = 0
-#     result[i][i] = "-"
-
-print(graph)
-
-
-def dijkstra(start):
-    q = []
-    distance = [INF] * (N+1)
-
-    heapq.heappush(q, (0, start))
-    distance[start] = 0
-
-    while q:
-        dist, now = heapq.heappop(q)
-        print(graph[1])
-        for i in graph[now]:
-            if distance[i[0]] <= dist:
-                continue
-            if distance[i[0]] > distance[now]+i[1]:
-                distance[i[0]] = distance[now]+i[1]
-                heapq.heappush(q, (distance[now]+i[0], i[1]))
-    print(distance)
-
-
-dijkstra(1)
-
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        d
-        if i == j:
-            continue
-        if distance[i][j] > distance[i][k]+distance[k][j]:
-            distance[i][j] = distance[i][k]+distance[k][j]
-            result[i][j] = k
-        elif distance[i][j] == distance[i][k]+distance[k][j]:
-            if result[i][j] > k:
-                result[i][j] = k
-for i in range(1, N+1):
-    print(distance[i])
-print("------")
-for i in range(1, N+1):
-    print(result[i])
