8edb197b670a81b50f3629b59920ea9c5181e465
EUNJEONGMUN <munej26@gmail.com>
2022-04-19 15:35:27 +0900

    [ADD] #28 EUN_ing 220419 commit

--- eun/Programmers/조이스틱.py
+++ eun/Programmers/조이스틱.py
@@ -1,8 +1,6 @@
 def solution(name):
-    alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
-             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    # 'A' == 65
-    move = 0
+    move_right = 0
+    move_left = 0
     now = 0
 
     for i in range(len(name)):
@@ -10,17 +8,39 @@ def solution(name):
             continue
 
         if ord(name[i])-ord("A") < 14:
-            move += ord(name[i])-ord("A")
+            move_right += ord(name[i])-ord("A")
         else:
-            move += 26-(ord(name[i])-ord("A"))
+            move_right += 26-(ord(name[i])-ord("A"))
 
-        move += 1
+        move_right += 1
 
-    if len(name) > 1:
-        if name[0] == "A" or name[1] == "A":
-            move -= 1
+    if ord(name[0])-ord("A") < 14:
+        move_left += ord(name[0])-ord("A")
     else:
-        if name[0] == "A":
-            move -= 1
+        move_left += 26-(ord(name[0])-ord("A"))
 
-    return move
+    move_left += 1
+
+    for i in range(len(name)-1, 0, -1):
+        if name == "A":
+            continue
+
+        if ord(name[i])-ord("A") < 14:
+            move_left += ord(name[i])-ord("A")
+        else:
+            move_left += 26-(ord(name[i])-ord("A"))
+
+        move_left += 1
+
+    # if len(name) > 1:
+    #     if name[0] == "A" or name[1] == "A":
+    #         move -= 1
+    # else:
+    #     if name[0] == "A":
+    #         move -= 1
+
+    return min(move_left, move_right)-1
+
+
+print(solution("JEROEN"))
+print(solution("JAN"))
