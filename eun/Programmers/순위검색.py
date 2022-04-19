d7fe344bf58cb5126a967426e8b84dda1ada93c2
EUNJEONGMUN <munej26@gmail.com>
2022-04-19 15:35:10 +0900

    [ADD] #28 EUN_complete 220419 commit

--- eun/Programmers/순위검색.py
+++ eun/Programmers/순위검색.py
@@ -1,34 +1,5 @@
-# def solution(info, query):
-#     info_list = [i.split() for i in info]
-
-#     result = []
-
-#     for i in query:
-#         q = i.split()
-#         count = 0
-#         for j in range(len(info_list)):
-#             if q[0] != "-":
-#                 if info_list[j][0] != q[0]:
-#                     continue
-#             if q[2] != "-":
-#                 if info_list[j][1] != q[2]:
-#                     continue
-
-#             if q[4] != "-":
-#                 if info_list[j][2] != q[4]:
-#                     continue
-
-#             if q[6] != "-":
-#                 if info_list[j][3] != q[6]:
-#                     continue
-
-#             if int(info_list[j][4]) < int(q[7]):
-#                 continue
-
-#             count += 1
-#         result.append(count)
-#     return result
-
+# 순위검색
+# 2022-04-16
 
 def find_people(array, target):
     left, right = 0, len(array)
@@ -86,6 +57,37 @@ def solution(infos, queries):
 
     return answer
 
+
+# def solution(info, query):
+#     info_list = [i.split() for i in info]
+
+#     result = []
+
+#     for i in query:
+#         q = i.split()
+#         count = 0
+#         for j in range(len(info_list)):
+#             if q[0] != "-":
+#                 if info_list[j][0] != q[0]:
+#                     continue
+#             if q[2] != "-":
+#                 if info_list[j][1] != q[2]:
+#                     continue
+
+#             if q[4] != "-":
+#                 if info_list[j][2] != q[4]:
+#                     continue
+
+#             if q[6] != "-":
+#                 if info_list[j][3] != q[6]:
+#                     continue
+
+#             if int(info_list[j][4]) < int(q[7]):
+#                 continue
+
+#             count += 1
+#         result.append(count)
+#     return result
 
 print(solution(["java backend junior pizza 150", "python frontend senior chicken 210", "python frontend senior chicken 150", "cpp backend senior pizza 260", "java backend junior chicken 80", "python backend senior chicken 50"], [
       "java and backend and junior and pizza 100", "python and frontend and senior and chicken 200", "cpp and - and senior and pizza 250", "- and backend and senior and - 150", "- and - and - and chicken 100", "- and - and - and - 150"]))
